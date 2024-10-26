89d5673c0253910428920ec0d6ac0e3454dc46ac
Youssef548 <youssefahmedpvp@gmail.com>
2024-10-26 15:18:37 +0300

    Update heading text in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React V2 ya dasdad</h1>
+      <h1>Vite + React V2 ya should not included on APP2</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}

2ecb6f652db5e5dd15adaed2f83c2d7c66992a69
Youssef548 <youssefahmedpvp@gmail.com>
2024-10-26 12:29:07 +0300

    Update heading in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React V2 ya brooo</h1>
+      <h1>Vite + React V2 ya brooo asdasdsdda sd</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
